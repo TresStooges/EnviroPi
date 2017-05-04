f495b4d5826066bda337db8f40e3de353aefe265
John Shepler <john.sheplerjr@gmail.com>
2017-05-04 10:42:14 -0600

    MVP

--- public/js/routes/location.js
+++ public/js/routes/location.js
@@ -2,25 +2,35 @@
     'use strict'
 
     angular.module('app').component('environment', {
-        controller: function(dataService, $stateParams) {
+        controller: function(dataService, $stateParams, $http) {
             const vm = this
-
+            vm.location = location()
+            console.log(vm.location);
             vm.$onInit = function() {
-                const locationId = $stateParams.location
-                vm.location = dataService.findById(locationId)
-                console.log(vm.location);
+                var ourShit = dataService.location
+                // const locationId = $stateParams.location
+                //  vm.location = dataService.findById(locationId)
             }
+
+            function location(){
+
+                return dataService.location
+            }
+            // console.log(dataService.location);
+
+
+
         },
         template: `
         <main class="locationMain">
             <div class="locationArea">
-                <h1>{{$ctrl.location.name}}</h1>
-                <img class="liveImg" src="{{$ctrl.location.imgPath}}">
+                <h1>{{$ctrl.location[$ctrl.location.length -1].name}}</h1>
+                <img class="liveImg" src="{{$ctrl.location[$ctrl.location.length -1].image}}">
                 <h4>Date/Time taken</h4>
-                <h1>{{$ctrl.location.dateTime | amDateFormat:'MMMM Do YYYY'}}</h4>
-                <h1>{{$ctrl.location.dateTime | amDateFormat:'h:mm a'}}</h1>
+                <h1>{{$ctrl.location[$ctrl.location.length -1].timestamp | amDateFormat:'MMMM Do YYYY'}}</h4>
+                <h1>{{$ctrl.location[$ctrl.location.length -1].timestamp | amDateFormat:'h:mm a'}}</h1>
                 <h4>Temperature</h4>
-                <h1>{{$ctrl.location.temperature}}&#176;f</h1>
+                <h1>{{$ctrl.location[$ctrl.location.length -1].temperature}}&#176;f</h1>
             </div>
         </main>
 
--- public/js/services/dataService.js
+++ public/js/services/dataService.js
@@ -2,30 +2,52 @@
     'use strict'
 
     angular.module('app')
-    .service('dataService', function() {
-            this.location = [
-                {
-                    id: 1,
-                    location: 1,
-                    name: 'Veg Room1',
-                    dateTime: new Date(),
-                    temperature: 75.5,
-                    imgPath: 'https://s3-us-west-2.amazonaws.com/enviropitresstooges/customname3.png'
-                },
-                {
-                    id: 2,
-                    location: 2,
-                    name: 'Flower Room',
-                    dateTime: new Date(),
-                    temperature: 68.8,
-                    imgPath: 'https://static.pexels.com/photos/4825/red-love-romantic-flowers.jpg'
-                }
+    .service('dataService', service)
 
-            ]
+    service.$inject = ['$http']
+
+    function service ($http) {
+        const vm = this
+        this.location = []
+        $http.get('https://enviropi-backend.herokuapp.com/environment')
+            .then(function(response){
+                response.data.forEach(element => {
+                    vm.location.push(element)
+                })
+            })
+            .catch('Not working')
 
             this.findById = function (locations) {
                 return this.location.find(location => location.location == locations)
             }
-    })
+
+    }
+
+
+
+
+
+
+            // this.location = [
+            //     {
+            //         id: 1,
+            //         location: 1,
+            //         name: 'Veg Room1',
+            //         dateTime: new Date(),
+            //         temperature: 75.5,
+            //         imgPath: 'https://s3-us-west-2.amazonaws.com/enviropitresstooges/customname3.png'
+            //     },
+            //     {
+            //         id: 2,
+            //         location: 2,
+            //         name: 'Flower Room',
+            //         dateTime: new Date(),
+            //         temperature: 68.8,
+            //         imgPath: 'https://static.pexels.com/photos/4825/red-love-romantic-flowers.jpg'
+            //     }
+            //
+            // ]
+
+    // })
 
 }());
